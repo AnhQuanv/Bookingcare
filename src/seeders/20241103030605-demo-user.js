e2df3efc28bdf8888460ba27e081d08a97d96984
Banhkute <anhquanvo911@gmail.com>
2024-11-03 10:07:49 +0700

    Create database with Sequelize Cli

---
+++ src/seeders/20241103030605-demo-user.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'John1',
+        lastName: 'Doe1',
+        email: 'example1@example.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
