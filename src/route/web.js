ee786902330da652bc9107dd0ef888c7345398ac
Banhkute <anhquanvo911@gmail.com>
2024-11-29 17:09:44 +0700

    Add API route for deleting a user

--- src/route/web.js
+++ src/route/web.js
@@ -21,7 +21,7 @@ let initWebRoutes = (app) => {
     router.post('/api/login', userController.handleLogin)
     router.get('/api/get-all-users', userController.handleGetAllUser);
     router.post('/api/create-new-user', userController.handleCreateNewUser)
-
+    router.delete('/api/delete-user', userController.handleDeleteUser)
 
 
 
